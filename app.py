0c77d7a3d1d628556543b78f3eced1aefbaca3f1
Sarah Robinson <robin26s@mtholyoke.edu>
2014-11-29 17:46:08 -0500

    Add initial webpage and server without api key

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+from readgeodata import OsmDataReader
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    if request.method == "GET":
+        datareader = OsmDataReader("mhc.osm.xml")
+        searchnodes = datareader.createSearchGraph()
+        return render_template("home.html",graphnodes=searchnodes)
+    else:
+        return "POST requests not yet supported."
+    
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
