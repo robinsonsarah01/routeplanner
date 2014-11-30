1c573fbc578e9ce9140e8264e2a96fff2cac9673
Sarah Robinson <robin26s@mtholyoke.edu>
2014-11-30 15:48:25 -0500

    Complete display of form to start search

--- app.py
+++ app.py
@@ -2,14 +2,19 @@ from flask import Flask, render_template, request
 from readgeodata import OsmDataReader
 app = Flask(__name__)
 
+print "starting data reader"
+#datareader = OsmDataReader("southhadley.osm.xml")
+datareader = OsmDataReader("mhc.osm.xml")
+print "started data reader, initing graph"
+searchnodes = datareader.createSearchGraph()
+print "inited graph"
+
 @app.route('/', methods=["GET","POST"])
 def home():
     if request.method == "GET":
-        datareader = OsmDataReader("mhc.osm.xml")
-        searchnodes = datareader.createSearchGraph()
         return render_template("home.html",graphnodes=searchnodes)
     else:
-        return "POST requests not yet supported."
+        return "POST requests not yet supported. This will eventually be a search result."
     
 if __name__ == "__main__":
     app.debug = True
