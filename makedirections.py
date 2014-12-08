4de17b8b50476ba94331d7e2e75a9bddc9c35826
Grace Seungin Yoo <yoo22g@mtholyoke.edu>
2014-12-07 19:15:11 -0500

    Revert "Revert "debugging routesearch, add routesearch to test for makedirections""
    This reverts commit e470563fb04cf8be88d7799b0bf30e33317e2e8a.

--- makedirections.py
+++ makedirections.py
@@ -1,7 +1,8 @@
 from treeelements import *
 import math
 # below only used in testing, remove later
-from readgeodata import OsmDataReader
+from readgeodata import *
+import routesearch
 
 def generateDirections(nodelist):
     """
@@ -140,7 +141,8 @@ def getBearing(start,end):
     
 if __name__ == "__main__":
     # create test list
-    testids = ["72088866","1354509391","2133302537","72053830","1353506942","2133302496","72062153","72075396","72084662","72014487","71987051","72066712"] # these aren't actually all neighbours
+    #testids = ["72088866","1354509391","2133302537","72053830","1353506942","2133302496","72062153","72075396","72084662","72014487","71987051","72066712"] # these aren't actually all neighbours
+    testids = routesearch.startSearch(20)
     mhcdata = OsmDataReader("mhc.osm.xml")
     searchnodes = mhcdata.createSearchGraph()
     testlist = [ searchnodes[id] for id in testids ]
